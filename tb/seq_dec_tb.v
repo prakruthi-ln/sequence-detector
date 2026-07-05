f4f28e99792ff41030091e4bebb3140d9e9dd1b2
prakruthi-ln <prakruthiln12@gmail.com>
2026-07-05 13:20:59 +0530

    Update seq_dec_tb.v

--- tb/seq_dec_tb.v
+++ tb/seq_dec_tb.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
@@ -21,23 +22,18 @@
 
 
 module seq_dec_tb;
-
 reg clk, rst, in;
 wire out;
-
 seq_dec uut (.in(in), .clk(clk), .rst(rst), .out(out));
-
-// clock
 always #5 clk = ~clk;
 
 initial begin
     clk = 0;
     rst = 1;
     in = 0;
-
     #10 rst = 0;
-
-    // Input: 1011011
+   
+ $display("normal sequence");
     in = 1; #10;
     in = 0; #10;
     in = 0; #10;
@@ -45,8 +41,44 @@ initial begin
     in = 0; #10;
     in = 1; #10;
     in = 1; #10;
-
-   #20 $finish;
+ $display("--overlapping sequence--");
+    in = 1; #10;
+    in = 0; #10;
+    in = 1; #10;
+    in = 1; #10;
+    in = 0; #10;
+    in = 1; #10;
+    in = 1; #10;
+ $display("--all zeroes--");
+    in = 0; #10;
+    in = 0; #10;
+    in = 0; #10;
+    in = 0; #10;
+    in = 0; #10;
+    in = 0; #10;
+    in = 0; #10;
+ $display("--all ones--");
+    in = 1; #10;
+    in = 1; #10;
+    in = 1; #10;
+    in = 1; #10;
+    in = 1; #10;
+    in = 1; #10;
+    in = 1; #10;
+    
+ $display("--reset in middle--");
+    in = 1; #10;
+    in = 0; #10;
+    in = 1; #10;
+    rst=1; #10;
+    rst=0;  #10;
+    in = 1; #10;
+    in = 0; #10;
+    in = 1; #10;
+    in = 1; #10;
+    
+#50;
+$display(" all the cases are done");
+$finish;
 end
-
 endmodule
